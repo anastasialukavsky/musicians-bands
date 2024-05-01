e936d4c6871db9ad84a63bf2dc7975d3c9c64dbd
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-01 18:24:24 -0400

    add tests

--- index.test.js
+++ index.test.js
@@ -1,44 +1,78 @@
-const { sequelize } = require('./db');
-const { Band, Musician, Song } = require('./index')
+const { db } = require('./db');
+const { Band, Musician, Song } = require('./index');
 
 describe('Band, Musician, and Song Models', () => {
-    /**
-     * Runs the code prior to all tests
-     */
-    beforeAll(async () => {
-        // the 'sync' method will create tables based on the model class
-        // by setting 'force:true' the tables are recreated each time the 
-        // test suite is run
-        await sequelize.sync({ force: true });
-    })
 
-    test('can create a Band', async () => {
-        // TODO - test creating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  beforeAll(async () => {
+ 
+    await db.sync({ force: true });
+  });
 
-    test('can create a Musician', async () => {
-        // TODO - test creating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test('can create a Band', async () => {
+    const band = await Band.create({
+      name: 'The Beatles',
+      genre: 'Rock',
+    });
 
-    test('can update a Band', async () => {
-        // TODO - test updating a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+    expect(band.name).toBe('The Beatles');
+    expect(band.genre).toBe('Rock');
+  });
 
-    test('can update a Musician', async () => {
-        // TODO - test updating a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+  test('can create a Musician', async () => {
+    const musician = await Musician.create({
+      name: 'John Lennon',
+      instrument: 'Guitar',
+    });
 
-    test('can delete a Band', async () => {
-        // TODO - test deleting a band
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
+    expect(musician.name).toBe('John Lennon');
+    expect(musician.instrument).toBe('Guitar');
+  });
 
-    test('can delete a Musician', async () => {
-        // TODO - test deleting a musician
-        expect('NO TEST').toBe('EXPECTED VALUE HERE');
-    })
-})
+  test('can create a Song', async () => {
+    const song = await Song.create({
+      title: 'Hey Jude',
+      year: 1968,
+      length: 7,
+    });
+
+    expect(song.title).toBe('Hey Jude');
+    expect(song.year).toBe(1968);
+    expect(song.length).toBe(7);
+  });
+
+  test('can update a Band', async () => {
+    const updatedBand = await Band.findByPk(1);
+    updatedBand.name = 'The Rolling Stones';
+    await updatedBand.save();
+
+    const band = await Band.findByPk(1);
+
+    expect(band.name).toBe('The Rolling Stones');
+  });
+
+  test('can update a Musician', async () => {
+    const updatedMusician = await Musician.findByPk(1);
+    updatedMusician.instrument = 'Drums';
+    await updatedMusician.save();
+
+    const musician = await Musician.findByPk(1);
+
+    expect(musician.instrument).toBe('Drums');
+  });
+
+  test('can delete a Band', async () => {
+    const deletedBand = await Band.findByPk(1);
+    await deletedBand.destroy();
+
+    const band = await Band.findByPk(1);
+    expect(band).toBeNull();
+  });
+
+  test('can delete a Musician', async () => {
+    const deletedMusician = await Musician.findByPk(1);
+    await deletedMusician.destroy();
+
+    const musician = await Musician.findByPk(1);
+    expect(musician).toBeNull();
+  });
+});
