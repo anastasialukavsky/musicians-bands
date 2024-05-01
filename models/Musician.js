3ff43290be8bacfb5b875961d332a52c37c671c9
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-01 16:24:06 -0400

    add musician model

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,15 @@
-const {Sequelize, sequelize} = require('../db');
+const {Sequelize, db} = require('../db');
+const DataTypes = require('sequelize')
 
 // TODO - define the Musician model
-let Musician;
+let Musician = db.define('musician', {
+    name: {
+        type: DataTypes.STRING
+    },
+    instrument: {
+        type: DataTypes.STRING
+    }
+})
 
 module.exports = {
     Musician
