0c21c2f0166567a74e8d1205421e914dc642c000
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-01 16:24:13 -0400

    add song model

--- models/Song.js
+++ models/Song.js
@@ -1,8 +1,19 @@
-const {Sequelize, sequelize} = require('../db');
+const { Sequelize, db } = require('../db');
+const DataTypes = require('sequelize');
 
 // TODO - define the Song model
-let Song;
+let Song = db.define('song', {
+  title: {
+    type: DataTypes.STRING,
+  },
+  year: {
+    type: DataTypes.STRING,
+  },
+  length: {
+    type: DataTypes.NUMBER,
+  },
+});
 
 module.exports = {
-    Song
+  Song,
 };
