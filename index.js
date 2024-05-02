c30e75938ada22849808c71ca9e66b2b5e6a09fc
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-02 14:58:49 -0400

    define associations

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const { Song } = require("./models/Song")
 // Define associations here
 
 
+Band.hasMany(Musician)
+Musician.belongsTo(Band)
+
+Band.belongsToMany(Song, { through: 'BandSong' }); 
+Song.belongsToMany(Band, { through: 'BandSong' });
+
+
 
 module.exports = {
     Band,
